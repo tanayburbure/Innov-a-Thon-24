8d66fcaa8efa300ac8fa9cace72a925c6fcbf39a
tanayburbure <tanayburbure@gmail.com>
2024-10-11 04:34:09 +0530

    Updates

---
+++ .history/src/components/Landingpage_20241011024940.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Navbar from './Navbar'
+export const Landingpage = () => {
+  return (
+    <div>
+        <div className='relative h-[100vh] w-full overflow-hidden'>
+            <video className='h-full w-full object-cover brightness-[50%]' autoPlay muted src="public\video\back.mp4"></video>
+            <div className='absolute inset-0 leading-normal flex flex-col '>
+                <Navbar className="absolute"/>
+                <h1 className='flex-grow tracking-tighter mr-4 font-semibold text-[9vw] text-center mt-[19vh] bg-[#0D7C66] to-green-100 text-transparent bg-clip-text'>Innov-a-Thon'24</h1>
+                <h4 className='text-transparent text-[2.7vh] font-semibold text-center ml-[40vw] mb-[65vh] bg-gradient-to-r from-green-900 to-green-100 bg-clip-text'>Igniting Innovation, Fueling the Future</h4>
+                <h3 className='p-12 absolute bottom-0 text-zinc-400 text-[2.3vh]'>
+                  DATE:
+                  <br />
+                  22nd December 2024</h3>
+
+                <h3 className='p-12 absolute -bottom-8 right-0 text-zinc-400 text-[2.3vh] text-right w-[30vw]'>
+                VENUE:
+                <br />
+                 Department of Electronics & Telecommunication, 
+                <br />
+                 AISSMS IOIT
+                </h3>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Landingpage
---
+++ .history/src/components/Navbar_20241011024807.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex '>
+      <div className='p-4 fixed'>
+        <img className='w-[4vw]' src="public\images\lobo.png" alt="" />
+      </div>
+      <div>
+        <h3>About</h3>
+        <h3>FAQ</h3>
+        <h3>Sponsors</h3>
+        <h3>Prizes</h3>
+        <span>
+          <button className='px-3 py-1 bg-[#0D7C66]'>Register here</button>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
